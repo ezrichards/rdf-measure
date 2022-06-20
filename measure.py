4923a5e89afb21b0c2dd5a5c065c01cff792eaee
Ethan Richards <ethanrichards22@gmail.com>
2022-06-20 11:50:50 -0600

    Add graph serialization

--- measure.py
+++ measure.py
@@ -1,17 +1,37 @@
 # Measurement apparatus for RDF encodings
 import sys
+import os
+import csv
+import datetime
+import rdflib
 
 file_name = input("Specify the file to measure: ")
-print("File name is: " + file_name)
-
 try:
     file = open(file_name)
 except OSError:
     print("Could not open file: " + file_name)
     sys.exit()
 
-with file:
-    lines = file.readlines()
+graph = rdflib.Graph()
+graph.parse(file_name)
 
-    for line in lines: 
-        print(line)
+results_folder = "results-" + datetime.datetime.now().strftime("%m-%d-%Y")
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+formats = ["ttl", "xml", "json-ld", "ntriples", "n3", "trig"]
+metadata = [file_name, len(graph)] # original file and number of triples
+
+with open(results_folder + '/results.csv', 'w') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(metadata)
+
+    for graph_format in formats:
+        serialized_file = results_folder + "/graph." + graph_format
+
+        graph.serialize(format=graph_format, destination=serialized_file, encoding="utf-8")
+
+        # save as format, size (in bytes)
+        writer.writerow([graph_format, os.path.getsize(serialized_file)])
+
+print(f"Results generated at {results_folder}")
